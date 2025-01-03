d01946a152dcace10f85f84f4f1ab18aeb6e7aab
Laxmi S Kallimath <38729902+laxmiskallimath@users.noreply.github.com>
2025-01-04 00:24:27 +0530

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -3,12 +3,11 @@ import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
-st.title(":cup_with_straw: Pending Smoothie Orders! :cup_with_straw:")
+st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
 st.write(
-    """Orders that need to be filled""")
-
-name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your Smoothie will be:', name_on_order)
+    """Orders that need to be filled
+    """
+)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -17,23 +16,19 @@ my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED
 if my_dataframe:
     editable_df = st.data_editor(my_dataframe)
     submitted = st.button('Submit')
-    if submitted:  
-
+    if submitted:
+         #session.sql(my_insert_stmt).collect()
+            
         og_dataset = session.table("smoothies.public.orders")
         edited_dataset = session.create_dataframe(editable_df)
     
         try:
             og_dataset.merge(edited_dataset
-                        , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                        , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                        )
-            st.success("Someone clicked the button.", icon="👍")
+                         , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                         , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                         )
+            st.success("Order(s) updated!", icon="👍")
         except:
             st.write('Something went wrong.')
-
 else:
     st.success('There are no pending orders right now', icon="👍")
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
