c313f4caf7c3b3bf060c27b690dbbb1c883fdf7c
Laxmi S Kallimath <38729902+laxmiskallimath@users.noreply.github.com>
2025-01-04 00:27:52 +0530

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -1,37 +1,33 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
-
+from snowflake.snowpark.functions import col, when_matched
 # Write directly to the app
-st.title(":cup_with_straw: Pending Smoothies Orders!:cup_with_straw:")
-st.write(
-    """Orders that need to be filled.
-    """)
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
-editable_df = st.data_editor(my_dataframe)
 
-submitted = st.button('Submit')
 
-if submitted:
-    og_dataset = session.table("smoothies.public.orders")
-    edited_dataset = session.create_dataframe(editable_df)
+cnx = st.connection("snowflake")
+session = cnx.session()
 
-    try:
-        og_dataset.merge(
-            edited_dataset,
-            (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID']),
-            [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-        )
-        st.success('Order(s) Updated', icon='👍')
-    except:
-        st.write('Something went wrong')
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==False).collect()
+
+st.text('Orders that need to filled')
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit') 
+
+    if submitted:
+
+        try:
+            og_dataset = session.table("smoothies.public.orders")
+            edited_dataset = session.create_dataframe(editable_df)
+            og_dataset.merge(edited_dataset
+                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+            st.success('Someone clicked the button', icon = '👍')
+        except:
+            st.write('Something went wrong.')
+
 else:
-    st.success('There are no pending orders right now',icon="👍")
- 
-
-
-
-
+    st.success('There are no pending orders right now',icon = '👍' )
