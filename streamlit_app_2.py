003d0e95295792dbc1613bf7fa0af5de56416c43
Laxmi S Kallimath <38729902+laxmiskallimath@users.noreply.github.com>
2025-01-04 00:25:27 +0530

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -1,14 +1,14 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col, when_matched
+#from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col ,when_matched
 
 # Write directly to the app
-st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
-st.write(
-    """Orders that need to be filled
-    """
-)
+st.title(":cup_with_straw: Pending Smoothie Order :cup_with_straw:")
+st.write("Order that need to be filled.")
 
+
+#session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
@@ -17,18 +17,15 @@ if my_dataframe:
     editable_df = st.data_editor(my_dataframe)
     submitted = st.button('Submit')
     if submitted:
-         #session.sql(my_insert_stmt).collect()
-            
         og_dataset = session.table("smoothies.public.orders")
         edited_dataset = session.create_dataframe(editable_df)
-    
-        try:
+        try:  
             og_dataset.merge(edited_dataset
-                         , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                         , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                         )
-            st.success("Order(s) updated!", icon="👍")
+                 , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                 , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                )
+            st.success('Order(s) updated', icon = '👍')
         except:
-            st.write('Something went wrong.')
+            st.success('Something went wrong')
 else:
-    st.success('There are no pending orders right now', icon="👍")
+    st.success('No pending order now ', icon = '👍')
